0961c3d9fbe0cc4e1e5bdc7de82499e24c559e90
Blayne Chard <bchard@linz.govt.nz>
2022-10-11 16:36:46 +1300

    feat!: allow multiple layers to be downloaded with the layer downloader
    This changes the interface from `--layerId :layerId --version :version` to `layerId@version`

--- src/commands/lds-cache/lds.cache.ts
+++ src/commands/lds-cache/lds.cache.ts
@@ -1,5 +1,5 @@
 import { fsa } from '@chunkd/fs';
-import { command, option, optional, string } from 'cmd-ts';
+import { command, option, restPositionals, string } from 'cmd-ts';
 import * as stac from 'stac-ts';
 import { createGunzip } from 'zlib';
 import { logger } from '../../log.js';
@@ -15,38 +15,38 @@ export const commandLdsFetch = command({
   args: {
     config,
     verbose,
-    version: option({ type: optional(string), long: 'version', description: 'Layer version to download' }),
-    layerId: option({ type: string, long: 'layer-id', description: 'Layer to download' }),
+    layers: restPositionals({ type: string, description: 'Layer id and optional version "layer@version"' }),
     target: option({ type: string, long: 'target', description: 'Target location to save file' }),
   },
   handler: async (args) => {
     registerCli(args);
 
-    const layerId = args.layerId;
-    const layerVersion = args.version;
+    for (const layer of args.layers) {
+      const [layerId, layerVersion] = layer.split('@');
 
-    if (isNaN(Number(layerId))) throw new Error('Invalid LayerId:' + layerId);
-    logger.info({ layerId, layerVersion }, 'Collection:Download:Start');
+      if (isNaN(Number(layerId))) throw new Error('Invalid LayerId:' + layerId);
+      logger.info({ layerId, layerVersion }, 'Collection:Download:Start');
 
-    if (args.version != null) {
-      if (isNaN(Number(layerVersion))) throw new Error('Invalid LayerVersion:' + layerVersion);
-      const source = `s3://linz-lds-cache/${layerId}/${layerId}_${layerVersion}.gpkg`;
-      await fsa.head(source); // Ensure we have read permission for the source
+      if (layerVersion != null) {
+        if (isNaN(Number(layerVersion))) throw new Error('Invalid LayerVersion:' + layerVersion);
+        const source = `s3://linz-lds-cache/${layerId}/${layerId}_${layerVersion}.gpkg`;
+        await fsa.head(source); // Ensure we have read permission for the source
 
-      logger.info({ layerId, layerVersion, source }, 'Collection:Item:Fetch');
-      await fsa.write(args.target ?? `./${layerId}_${layerVersion}.gpkg`, fsa.stream(source).pipe(createGunzip()));
+        logger.info({ layerId, layerVersion, source }, 'Collection:Item:Fetch');
+        await fsa.write(args.target ?? `./${layerId}_${layerVersion}.gpkg`, fsa.stream(source).pipe(createGunzip()));
+      }
+
+      const collectionJson = await fsa.readJson<stac.StacCollection>(`s3://linz-lds-cache/${layerId}/collection.json`);
+      logger.info({ layerId, title: collectionJson.title }, 'Collection:Download:Done');
+
+      const lastItem = collectionJson.links.filter((f) => f.rel === 'item').pop();
+      if (lastItem == null) throw new Error('No items found');
+
+      const targetFile = args.target ?? lastItem.href.replace('.json', '.gpkg');
+
+      const targetPath = getTargetPath(`s3://linz-lds-cache/${layerId}/`, lastItem.href).replace('.json', '.gpkg');
+      logger.info({ layerId, lastItem, source: targetPath }, 'Collection:Item:Fetch');
+      await fsa.write(targetFile, fsa.stream(targetPath).pipe(createGunzip()));
     }
-
-    const collectionJson = await fsa.readJson<stac.StacCollection>(`s3://linz-lds-cache/${layerId}/collection.json`);
-    logger.info({ layerId, title: collectionJson.title }, 'Collection:Download:Done');
-
-    const lastItem = collectionJson.links.filter((f) => f.rel === 'item').pop();
-    if (lastItem == null) throw new Error('No items found');
-
-    const targetFile = args.target ?? lastItem.href.replace('.json', '.gpkg');
-
-    const targetPath = getTargetPath(`s3://linz-lds-cache/${layerId}/`, lastItem.href).replace('.json', '.gpkg');
-    logger.info({ layerId, lastItem, source: targetPath }, 'Collection:Item:Fetch');
-    await fsa.write(targetFile, fsa.stream(targetPath).pipe(createGunzip()));
   },
 });
