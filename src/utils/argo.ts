0cbfc3039f3bbfa60de1dea2a6dbb30a25fb271c
Blayne Chard <bchard@linz.govt.nz>
2022-11-03 10:46:22 +1300

    fix: ensure loc.key is a string

--- src/utils/argo.ts
+++ src/utils/argo.ts
@@ -6,6 +6,7 @@
 export function getArgoLocation(): string | null {
   const loc = JSON.parse(process.env['ARGO_TEMPLATE'] ?? '{}')?.archiveLocation?.s3;
   if (loc == null) return null;
+  if (typeof loc.key !== 'string') return null;
 
   const key = loc.key.replace('{{pod.name}}', '');
   return `s3://${loc.bucket}/${key}`;
