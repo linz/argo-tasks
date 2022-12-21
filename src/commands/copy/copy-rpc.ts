a0b0379296fe0d625f27a2615b197f88b3d4ac31
Blayne Chard <bchard@linz.govt.nz>
2022-12-22 09:11:12 +1300

    docs: add comments around args to copy

--- src/commands/copy/copy-rpc.ts
+++ src/commands/copy/copy-rpc.ts
@@ -1,20 +1,31 @@
 export type CopyContract = {
-  copy: (args: CopyContractArgs) => Promise<CopyStats>;
+  copy(args: CopyContractArgs): Promise<CopyStats>;
 };
 
 export interface CopyContractArgs {
+  /** Copy ID for tracing */
   id: string;
+  /** List of files that need to be copied */
   manifest: { source: string; target: string }[];
+  /** Offset into the manifest to start at */
   start: number;
+  /** Number of records to copy */
   size: number;
+  /** Force overwrite files */
   force: boolean;
+  /** Do not overwrite existing files but skip them if they are the same size */
   noClobber: boolean;
 }
 
 export interface CopyStats {
+  /** Number of files copied */
   copied: number;
+  /** Number of bytes copied */
   copiedBytes: number;
+  /** Number of times a file was retried */
   retries: number;
+  /** Number of files that has been skipped, Generally because the file already exists */
   skipped: number;
+  /** Number of bytes that has been skipped. */
   skippedBytes: number;
 }
