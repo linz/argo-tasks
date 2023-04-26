736ef66db4a5d17cf86f11bd75f5aab56754b182
Blayne Chard <bchard@linz.govt.nz>
2023-04-26 16:13:49 +1200

    feat(stac-catalog): load collection titles from paths

--- src/commands/stac-catalog/__test__/stac.catalog.test.ts
+++ src/commands/stac-catalog/__test__/stac.catalog.test.ts
@@ -1,21 +1,46 @@
 import o from 'ospec';
-import { createLinks } from '../stac.catalog.js';
+import { createLinks, makeRelative } from '../stac.catalog.js';
+import { FsMemory } from '@chunkd/source-memory';
+import { fsa } from '@chunkd/fs';
 
-o.spec('stacCatalog', function () {
-  o('listLocation', async function () {
-    o(
-      createLinks(
-        ['./directory1/collection.json', './directory2/collection.json', ''],
-        [
-          { rel: 'self', href: './catalog.json' },
-          { rel: 'root', href: './catalog.json' },
-        ],
-      ),
-    ).deepEquals([
+o.spec('stacCatalog', () => {
+  const fs = new FsMemory();
+  o.beforeEach(() => {
+    fs.files.clear();
+    fsa.register('m://', fs);
+  });
+
+  o('listLocation', async () => {
+    await fs.write('m://base/directory1/collection.json', JSON.stringify({ title: 'CollectionA' }));
+    await fs.write('m://base/directory2/collection.json', JSON.stringify({ title: 'CollectionB' }));
+
+    const links = await createLinks('m://base/', [
       { rel: 'self', href: './catalog.json' },
       { rel: 'root', href: './catalog.json' },
-      { rel: 'child', href: './directory1/collection.json' },
-      { rel: 'child', href: './directory2/collection.json' },
     ]);
+    o(links).deepEquals([
+      { rel: 'self', href: './catalog.json' },
+      { rel: 'root', href: './catalog.json' },
+      { rel: 'child', href: './directory1/collection.json', title: 'CollectionA' },
+      { rel: 'child', href: './directory2/collection.json', title: 'CollectionB' },
+    ]);
+  });
+});
+
+o.spec('makeRelative', () => {
+  o('should make relative urls', () => {
+    o(makeRelative('s3://linz-imagery/', 's3://linz-imagery/catalog.json')).equals('catalog.json');
+  });
+
+  o('should make relative from absolute paths', () => {
+    o(makeRelative('/home/blacha/', '/home/blacha/catalog.json')).equals('catalog.json');
+  });
+
+  o('should make relative relative paths', () => {
+    o(makeRelative('/home/blacha/', './catalog.json')).equals('./catalog.json');
+  });
+
+  o('should not make relative on different paths', () => {
+    o(() => makeRelative('/home/blacha/', '/home/test/catalog.json')).throws(Error);
   });
 });
