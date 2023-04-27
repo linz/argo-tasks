7395c1f6bfd4596d11807fa4c4e9dc518878d3f7
Blayne Chard <bchard@linz.govt.nz>
2023-04-27 12:20:55 +1200

    feat(stac-catalog): load collection titles from paths (#389)

--- src/commands/stac-catalog/stac.catalog.ts
+++ src/commands/stac-catalog/stac.catalog.ts
@@ -1,8 +1,40 @@
 import { fsa } from '@chunkd/fs';
-import { command, option, string } from 'cmd-ts';
+import { command, option, positional, string } from 'cmd-ts';
+import { isAbsolute } from 'path';
 import * as st from 'stac-ts';
 import { logger } from '../../log.js';
 import { config, registerCli, verbose } from '../common.js';
+
+/** is a path a URL */
+export function isUrl(path: string): boolean {
+  try {
+    new URL(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+/**
+ * Convert a path to relative
+ *
+ * https://foo.com + https://foo.com/bar.html => ./bar.html
+ * s3://foo/ + s3://foo/bar/baz.html => ./bar/baz.html
+ * /home/blacha + /home/blacha/index.json => ./index.json
+ *
+ * @param basePath path to make relative to
+ * @param filePath target file
+ * @returns relative path to file
+ */
+export function makeRelative(basePath: string, filePath: string): string {
+  if (isUrl(filePath) || isAbsolute(filePath)) {
+    if (!filePath.startsWith(basePath)) {
+      throw new Error(`FilePaths are not relative base: ${basePath} file: ${filePath}`);
+    }
+    return filePath.slice(basePath.length);
+  }
+  return filePath;
+}
 
 export const commandStacCatalog = command({
   name: 'stac-catalog',
@@ -16,11 +48,7 @@ export const commandStacCatalog = command({
       description: 'JSON template file location for the Catalog metadata',
     }),
     output: option({ type: string, long: 'output', description: 'Output location for the catalog' }),
-    collections: option({
-      type: string,
-      long: 'collections',
-      description: 'Location of text file containing collection.json paths',
-    }),
+    path: positional({ type: string, description: 'Location to search for collection.json paths' }),
   },
 
   handler: async (args) => {
@@ -28,41 +56,31 @@ export const commandStacCatalog = command({
 
     logger.info('StacCatalogCreation:Start');
 
-    let catalog;
-    try {
-      catalog = await fsa.readJson<st.StacCatalog>(args.template);
-    } catch (e) {
-      logger.error({ e }, 'readCatalogTemplate:Error');
-      return;
-    }
+    const catalog = await fsa.readJson<st.StacCatalog>(args.template);
 
-    let collections;
-    try {
-      collections = (await fsa.read(args.collections)).toString('utf8').split(/\r?\n/);
-    } catch (e) {
-      logger.error({ e }, 'readCollectionsLinks:Error');
-      return;
-    }
+    const templateLinkCount = catalog.links.length;
 
-    catalog.links = createLinks(collections, catalog.links);
+    catalog.links = await createLinks(args.path, catalog.links);
 
-    try {
-      await fsa.write(args.output, JSON.stringify(catalog));
-    } catch (e) {
-      logger.error({ catalogId: catalog.id, e }, 'writeCatalog:Error');
-      return;
-    }
-
-    logger.info({ catalogId: catalog.id }, 'StacCatalogCreation:Done');
+    await fsa.write(args.output, JSON.stringify(catalog));
+    logger.info(
+      { catalogId: catalog.id, collections: catalog.links.length - templateLinkCount },
+      'StacCatalogCreation:Done',
+    );
   },
 });
 
-export function createLinks(collections: string[], templateLinks: st.StacLink[]): st.StacLink[] {
+export async function createLinks(basePath: string, templateLinks: st.StacLink[]): Promise<st.StacLink[]> {
+  const collections = await fsa.toArray(fsa.list(basePath));
+
   for (const coll of collections) {
-    if (coll.includes('collection.json')) {
+    if (coll.endsWith('/collection.json')) {
+      const relPath = makeRelative(basePath, coll);
+      const collection = await fsa.readJson<st.StacCollection>(coll);
       const collLink: st.StacLink = {
         rel: 'child',
-        href: coll,
+        href: fsa.join('./', relPath),
+        title: collection.title,
       };
       templateLinks.push(collLink);
     }
