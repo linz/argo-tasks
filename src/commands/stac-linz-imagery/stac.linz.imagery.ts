4a1171d33321ac774ee6996e292c053e5b5b00e1
Alice Fage <afage@linz.govt.nz>
2023-05-19 13:08:44 +1200

    fix: git commands

--- src/commands/stac-linz-imagery/stac.linz.imagery.ts
+++ src/commands/stac-linz-imagery/stac.linz.imagery.ts
@@ -37,8 +37,8 @@ export const commandStacLinzImagery = command({
     registerCli(args);
     logger.info('StacCollectionCreation:Start');
 
-    // source: s3://linz-workflow-artifacts/2023-04/25-ispi-manawatu-whanganui-2010-2011-0-4m-tttsb/flat/
-    // target s3://linz-imagery/manawatu-whanganui/manawatu-whanganui_2010-2011_0.4m/rgb/2193/
+    // example source: s3://linz-workflow-artifacts/2023-04/25-ispi-manawatu-whanganui-2010-2011-0-4m-tttsb/flat/
+    // example target s3://linz-imagery/manawatu-whanganui/manawatu-whanganui_2010-2011_0.4m/rgb/2193/
 
     // const git_author_name = process.env['GIT_AUTHOR_NAME'] ?? 'Imagery[bot]';
     // const git_author_email = process.env['GIT_AUTHOR_EMAIL'] ?? 'imagery@linz.govt.nz';
@@ -52,10 +52,8 @@ export const commandStacLinzImagery = command({
     const sourceCollection = `${sourceUrl.href.replace(/\/+$/, '')}/collection.json`;
     const targetCollection = `${gitRepo}stac${targetUrl.pathname.replace(/\/+$/, '')}/collection.json`;
     const targetPathParts = targetUrl.pathname.split('/');
-    const gitBranch = `feat(${targetPathParts[1]}): ${targetPathParts[2]}`;
-    console.log(sourceCollection);
-    console.log(targetCollection);
-    console.log(gitBranch);
+    const gitBranch = `feat(${targetPathParts[1]})/${targetPathParts[2]}`;
+    const gitCommitMsg = `feat(${targetPathParts[1]}): ${targetPathParts[2]}`;
 
     // const root: st.StacLink = '';
 
@@ -77,7 +75,7 @@ export const commandStacLinzImagery = command({
     // logger.info({ repository: gitRepo }, 'Git: Configure User Name');
     // execFileSync('git', ['config', 'user.name', gitName], { cwd: gitRepo }).toString().trim();
 
-    // const collection = await fsa.readJson<st.StacCatalog>(sourceCollection);
+    const collection = await fsa.readJson<st.StacCatalog>(sourceCollection);
 
     // console.log(collection.links);
 
@@ -90,67 +88,15 @@ export const commandStacLinzImagery = command({
     //             "type": "application/json",
     //         },
 
-    // for (const link in collection.links) {
-    // }
-
-    // for (let link in collection['links']) {
-    //   console.log(link);
-    //   if (link['rel'] === 'root') {
-    //     console.log(link);
-    //   }
-    // }
-
     //console.log(collection);
+
+    // Write the file
+
+    // Checkout branch
+    execFileSync('git', ['checkout', '-b', gitBranch], { cwd: gitRepo }).toString().trim();
+    // Add and commit
+    execFileSync('git', ['commit', '-am', gitCommitMsg], { cwd: gitRepo }).toString().trim();
+    // Push branch
+    execFileSync('git', ['push', 'origin', 'HEAD'], { cwd: gitRepo }).toString().trim();
   },
 });
-
-// # Rewrite the self link and save the file to its repo location
-// with open(tmp_collection_path) as cf:
-//     coll = json.load(cf)
-//     for link in coll["links"]:
-//         if link["rel"] == "root":
-//             coll["links"].remove(link)
-//     coll["links"].append(
-//         {
-//             "rel": "root",
-//             "href": "https://linz-imagery.s3.ap-southeast-2.amazonaws.com/catalog.json",
-//             "type": "application/json",
-//         },
-//     )
-
-//     # print(coll)
-
-//     write(
-//         "/tmp/tmp_collection.json",
-//         json.dumps(coll, ensure_ascii=False, indent=2).encode("utf-8"),
-//     )
-
-// # Checkout branch
-// run_command(["git", "checkout", "-b", "argo/" + git_branch_name], gitrepo)
-// # Add and commit
-// run_command(["git", "commit", "-am", "feat: " + git_branch_name], gitrepo)
-// # Push branch
-// run_command(["git", "push", "origin", "HEAD"], gitrepo)
-
-// export async function createLinks(basePath: string, templateLinks: st.StacLink[]): Promise<st.StacLink[]> {
-//   const collections = await fsa.toArray(fsa.list(basePath));
-
-//   for (const coll of collections) {
-//     if (coll.endsWith('/collection.json')) {
-//       const relPath = makeRelative(basePath, coll);
-//       const buf = await fsa.read(coll);
-//       const collection = JSON.parse(buf.toString()) as st.StacCollection;
-//       // Multihash header 0x12 - Sha256 0x20 - 32 bits of hex digest
-//       const checksum = '1220' + createHash('sha256').update(buf).digest('hex');
-//       const collLink: st.StacLink = {
-//         rel: 'child',
-//         href: fsa.join('./', relPath),
-//         title: collection.title,
-//         'file:checksum': checksum,
-//         'file:size': buf.length,
-//       };
-//       templateLinks.push(collLink);
-//     }
-//   }
-//   return templateLinks;
-// }
