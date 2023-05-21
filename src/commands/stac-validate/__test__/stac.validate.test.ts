f210baacc35e6e8b713861c330ba3a510dcf75ec
Blayne Chard <bchard@linz.govt.nz>
2023-05-22 10:16:09 +1200

    fix(stac-validate): support other schemes than http for links/references

--- src/commands/stac-validate/__test__/stac.validate.test.ts
+++ src/commands/stac-validate/__test__/stac.validate.test.ts
@@ -1,5 +1,5 @@
 import o from 'ospec';
-import { listLocation, iri, iriReference, getStacSchemaUrl, normaliseHref, isURL } from '../stac.validate.js';
+import { listLocation, getStacSchemaUrl, normaliseHref, isURL } from '../stac.validate.js';
 
 o.spec('stacValidate', function () {
   o('listLocation', async function () {
@@ -14,45 +14,36 @@ o.spec('stacValidate', function () {
       's3://example-bucket/test/item.json',
     ]);
   });
-  o('iriEmptyString', async function () {
-    o(iri('')).equals(false);
+
+  o.spec('getStacSchemaUrl', () => {
+    o('should return a item url', () => {
+      o(getStacSchemaUrl('Feature', '1.0.0', 'placeholder path')).equals(
+        'https://schemas.stacspec.org/v1.0.0/item-spec/json-schema/item.json',
+      );
+    });
+    o('should return a catalog url', () => {
+      o(getStacSchemaUrl('Catalog', '1.0.0', 'placeholder path')).equals(
+        'https://schemas.stacspec.org/v1.0.0/catalog-spec/json-schema/catalog.json',
+      );
+    });
+    o('should return a collection url', () => {
+      o(getStacSchemaUrl('Collection', '1.0.0', 'placeholder path')).equals(
+        'https://schemas.stacspec.org/v1.0.0/collection-spec/json-schema/collection.json',
+      );
+    });
+    o('should return null on invalid version', () => {
+      o(getStacSchemaUrl('Collection', '0.0.8', 'placeholder path')).equals(null);
+    });
+    o('should return null on invalid type', () => {
+      o(getStacSchemaUrl('CollectionItem', '1.0.0', 'placeholder path')).equals(null);
+    });
   });
-  o('iriString', async function () {
-    o(iri('test/')).equals(false);
-  });
-  o('iriStringHttp', async function () {
-    o(iri('https://schemas.stacspec.org/v1.0.0/item-spec/json-schema/item.json')).equals(true);
-  });
-  o('iriReferenceEmptyString', async function () {
-    o(iriReference('')).equals(false);
-  });
-  o('iriReferenceString', async function () {
-    o(iriReference('test/')).equals(false);
-  });
-  o('iriReferenceStringHttp', async function () {
-    o(iriReference('https://schemas.stacspec.org/')).equals(true);
-  });
-  o('getStacItemSchemaUrl', async function () {
-    o(getStacSchemaUrl('Feature', '1.0.0', 'placeholder path')).equals(
-      'https://schemas.stacspec.org/v1.0.0/item-spec/json-schema/item.json',
-    );
-  });
-  o('getStacCollectionSchemaUrl', async function () {
-    o(getStacSchemaUrl('Collection', '1.0.0', 'placeholder path')).equals(
-      'https://schemas.stacspec.org/v1.0.0/collection-spec/json-schema/collection.json',
-    );
-  });
-  o('getStacSchemaUrlInvalidStacVersion', async function () {
-    o(getStacSchemaUrl('Collection', '0.0.8', 'placeholder path')).equals(null);
-  });
-  o('getStacSchemaUrlInvalidStacType', async function () {
-    o(getStacSchemaUrl('CollectionItem', '1.0.0', 'placeholder path')).equals(null);
-  });
-  o('isURL', async function () {
+
+  o('isURL', () => {
     o(isURL('s3://test-bucket/test-survey/collection.json')).equals(true);
     o(isURL('data/test-survey/collection.json')).equals(false);
   });
-  o('normaliseHref', async function () {
+  o('normaliseHref', () => {
     o(normaliseHref('./item.json', 's3://test-bucket/test-survey/collection.json')).equals(
       's3://test-bucket/test-survey/item.json',
     );
@@ -63,3 +54,5 @@ o.spec('stacValidate', function () {
     o(normaliseHref('./item.json', 'collection.json')).equals('item.json');
   });
 });
+
+o.run();
