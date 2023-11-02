5035c199a50ce1b87a8f46c37829fcfa4100ceaa
Megan Davidson <mdavidson@linz.govt.nz>
2023-11-02 14:14:56 +1300

    fix: formatting

--- src/commands/lint/lint.constants.ts
+++ src/commands/lint/lint.constants.ts
@@ -1,22 +1,22 @@
 export const regions = [
-    'northland',
-    'auckland',
-    'waikato',
-    'bay-of-plenty',
-    'gisborne',
-    'hawkes-bay',
-    'taranaki',
-    'manawatu-whanganui',
-    'wellington',
-    'tasman',
-    'nelson',
-    'marlborough',
-    'west-coast',
-    'canterbury',
-    'otago',
-    'southland',
-  ];
+  'northland',
+  'auckland',
+  'waikato',
+  'bay-of-plenty',
+  'gisborne',
+  'hawkes-bay',
+  'taranaki',
+  'manawatu-whanganui',
+  'wellington',
+  'tasman',
+  'nelson',
+  'marlborough',
+  'west-coast',
+  'canterbury',
+  'otago',
+  'southland',
+];
 
-  export const imageryProducts = ['rgb']
+export const imageryProducts = ['rgb'];
 
-  export const imageryCrs = ['2193']
+export const imageryCrs = ['2193'];
