aba3c22fd9993c6ae1d6717e31aeb2983129a2f5
Megan Davidson <mdavidson@linz.govt.nz>
2023-11-06 12:52:25 +1300

    fix: fix and add tests

--- src/commands/lint/__test__/lint.test.ts
+++ src/commands/lint/__test__/lint.test.ts
@@ -9,6 +9,16 @@ describe('lintImageryPaths', () => {
       lintPath('s3://n-imagery/auckland/auckland_2012_0.075m/rgb/2193/');
     }, Error);
   });
+  it('Should Fail - Missing key (Product)', () => {
+    assert.throws(() => {
+      lintPath('s3://nz-imagery/auckland/auckland_2012_0.075m/2193/');
+    }, Error);
+  });
+  it('Should Fail - Extra args', () => {
+    assert.throws(() => {
+      lintPath('s3://nz-imagery/auckland/auckland_2012_0.075m/2193/extra-args/');
+    }, Error);
+  });
   it('Should Pass', () => {
     assert.ok(() => {
       lintPath('s3://nz-imagery/auckland/auckland_2012_0.075m/rgb/2193/');
@@ -17,29 +27,24 @@ describe('lintImageryPaths', () => {
 });
 
 describe('lintImageryPaths', () => {
-  it('Should Fail - Missing key', () => {
-    assert.throws(() => {
-      lintImageryPath(['nz-imagery', 'auckland', 'auckland_2012_0.075m', '2193', '']);
-    }, Error);
-  });
   it('Should Fail - Incorrect Region', () => {
     assert.throws(() => {
-      lintImageryPath(['nz-imagery', 'hawkesbay', 'hawkes-bay_2012_0.075m', 'rgb', '2193', '']);
+      lintImageryPath('hawkesbay', 'rgb', '2193');
     }, Error);
   });
   it('Should Fail - Incorrect product', () => {
     assert.throws(() => {
-      lintImageryPath(['nz-imagery', 'hawkes-bay', 'hawkes-bay_2012_0.075m', 'rgbi', '2193', '']);
+      lintImageryPath('hawkes-bay', 'rgbi', '2193');
     }, Error);
   });
   it('Should Fail - Incorrect Crs', () => {
     assert.throws(() => {
-      lintImageryPath(['nz-imagery', 'auckland', 'auckland_2012_0.075m', 'rgb', '219', '']);
+      lintImageryPath('auckland', 'rgb', '219');
     }, Error);
   });
   it('Should Pass', () => {
     assert.ok(() => {
-      lintImageryPath(['nz-imagery', 'auckland', 'auckland_2012_0.075m', 'rgb', '2193', '']);
+      lintImageryPath('auckland', 'rgb', '2193');
     });
   });
 });
