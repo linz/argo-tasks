7868de6b47b42a9f5d48d5522781454cdba16773
Megan Davidson <mdavidson@linz.govt.nz>
2023-11-14 14:16:55 +1300

    fix: lint resolution

--- src/commands/lint/__test__/lint.test.ts
+++ src/commands/lint/__test__/lint.test.ts
@@ -29,22 +29,27 @@ describe('lintImageryPaths', () => {
 describe('lintImageryPaths', () => {
   it('Should Fail - Incorrect Region', () => {
     assert.throws(() => {
-      lintImageryPath('/hawkesbay/hawkes-bay_2018_0-75m/rgb/2193/');
+      lintImageryPath('/hawkesbay/hawkes-bay_2018_0.75m/rgb/2193/');
     }, Error('region not in region list: hawkesbay'));
   });
   it('Should Fail - Incorrect product', () => {
     assert.throws(() => {
-      lintImageryPath('/hawkes-bay/hawkes-bay_2018_0-75m/rgbi/2193/');
+      lintImageryPath('/hawkes-bay/hawkes-bay_2018_0.75m/rgbi/2193/');
     }, Error('product not in product list: rgbi'));
   });
   it('Should Fail - Incorrect Crs', () => {
     assert.throws(() => {
-      lintImageryPath('/auckland/auckland_2020_0-2m/rgb/219/');
+      lintImageryPath('/auckland/auckland_2020_0.2m/rgb/219/');
     }, Error('crs not in crs list: 219'));
+  });
+  it('Should Fail - Trailing 0', () => {
+    assert.throws(() => {
+      lintImageryPath('/auckland/auckland_2020_0.20m/rgb/2193/');
+    }, Error('Trailing 0 in resolution of dataset name: auckland_2020_0.20m'));
   });
   it('Should Pass', () => {
     assert.ok(() => {
-      lintImageryPath('/auckland/auckland_2020_0-2m/rgb/2193/');
+      lintImageryPath('/auckland/auckland_2020_0.2m/rgb/2193/');
     });
   });
 });
