d5f02e4ca002bac9e82c779528200db2fe4e9796
Victor Engmark <vengmark@linz.govt.nz>
2024-01-19 09:16:27 +1300

    refactor: Use JSDoc formatting

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -13,10 +13,10 @@ export function slugify(input: string): string {
 }
 
 function removeDiacritics(input: string): string {
-  /*
-   Normalization form decomposition (NFD) splits characters like into their
-   [combining diacritical mark](https://www.unicode.org/charts/PDF/U0300.pdf) and the character which is being modified
-   by the diacritic. This way we can remove the macron from "ā", the accent from "é", and the like.
+  /**
+   * Normalization form decomposition (NFD) splits characters like into their
+   * [combining diacritical mark](https://www.unicode.org/charts/PDF/U0300.pdf) and the character which is being modified
+   * by the diacritic. This way we can remove the macron from "ā", the accent from "é", and the like.
    */
   const combiningDiacriticalMarks = /[\u0300-\u036F]/g;
   return input.normalize('NFD').replaceAll(combiningDiacriticalMarks, '');
