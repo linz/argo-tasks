888f973ba54cddbf6b742f8a8f7eccf4976deb7f
Victor Engmark <vengmark@linz.govt.nz>
2024-01-18 14:13:45 +1300

    refactor: Split out diacritics removal

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,10 +1,14 @@
 const combiningDiacriticalMarks = /[\u0300-\u036F]/g;
 
-export function slugify(input: string): string {
+function removeDiacritics(input: string): string {
   /*
    Normalization form decomposition (NFD) splits characters like into their
    [combining diacritical mark](https://www.unicode.org/charts/PDF/U0300.pdf) and the character which is being modified
    by the diacritic. This way we can remove the macron from "ā", the accent from "é", and the like.
    */
-  return input.normalize('NFD').replaceAll(combiningDiacriticalMarks, '').replaceAll(' ', '-').toLowerCase();
+  return input.normalize('NFD').replaceAll(combiningDiacriticalMarks, '');
+}
+
+export function slugify(input: string): string {
+  return removeDiacritics(input).replaceAll(' ', '-').toLowerCase();
 }
