b0a7874df994c473ccbfb0cb106886e4ea34bd50
Victor Engmark <vengmark@linz.govt.nz>
2024-01-19 15:15:52 +1300

    feat: Slugify strings TDE-1019 (#827)
    #### Motivation
    
    Allow us to easily slugify arbitrary strings for use in URLs.
    
    #### Modification
    
    This assumes that each part of a path will be slugified separately. This
    is less convenient than slugifying the entire path in one go, but
    ensures that no path segment contains a slash.
    
    #### Checklist
    
    _If not applicable, provide explanation of why._
    
    - [x] Tests updated
    - [x] Docs updated (comments only)
    - [x] Issue linked in Title

---
+++ src/utils/slugify.ts
@@ -0,0 +1,30 @@
+/**
+ * @param input Human-readable string
+ * @returns String slug. See src/utils/__test__/slugify.test.ts for examples.
+ */
+export function slugify(input: string): string {
+  const result = removeDiacritics(input).replaceAll('ø', 'o').replaceAll('Ø', 'O').replaceAll(' ', '-').toLowerCase();
+
+  const unhandledCharacters = result.match(/[^abcdefghijklmnopqrstuvwxyz0123456789_.-]/g);
+  if (unhandledCharacters) {
+    const sortedUniqueCharacters = Array.from(new Set(unhandledCharacters)).sort();
+    const formattedCharacters = sortedUniqueCharacters.map((character) => {
+      return JSON.stringify(character).replaceAll('\\\\', '\\');
+    });
+    throw Error(`Unhandled characters: ${formattedCharacters.join(', ')}`, {
+      cause: { characters: sortedUniqueCharacters },
+    });
+  }
+
+  return result;
+}
+
+/**
+ * Normalization form decomposition (NFD) splits characters like "ā" into their
+ * [combining diacritical mark](https://www.unicode.org/charts/PDF/U0300.pdf) and the character which is being modified
+ * by the diacritic. This way we can remove the macron from "ā", the accent from "é", and the like.
+ */
+function removeDiacritics(input: string): string {
+  const combiningDiacriticalMarks = /[\u0300-\u036F]/g;
+  return input.normalize('NFD').replaceAll(combiningDiacriticalMarks, '');
+}
