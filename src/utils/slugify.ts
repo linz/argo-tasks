51d9e0c84e64f9a7d2f878876f1c1aab2740a408
Victor Engmark <vengmark@linz.govt.nz>
2024-01-19 14:13:32 +1300

    docs: Add missing character

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -20,7 +20,7 @@ export function slugify(input: string): string {
 }
 
 /**
- * Normalization form decomposition (NFD) splits characters like into their
+ * Normalization form decomposition (NFD) splits characters like "ā" into their
  * [combining diacritical mark](https://www.unicode.org/charts/PDF/U0300.pdf) and the character which is being modified
  * by the diacritic. This way we can remove the macron from "ā", the accent from "é", and the like.
  */
