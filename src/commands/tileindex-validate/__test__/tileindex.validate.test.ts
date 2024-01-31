a885a55097058d76df53872ae1c3e3609d47c3b9
Victor Engmark <vengmark@linz.govt.nz>
2024-01-31 13:50:31 +1300

    refactor: Rename variable for clarity

--- src/commands/tileindex-validate/__test__/tileindex.validate.test.ts
+++ src/commands/tileindex-validate/__test__/tileindex.validate.test.ts
@@ -17,8 +17,8 @@ import { FakeCogTiff } from './tileindex.validate.data.js';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-function convertTileName(x: string, scale: number): string | null {
-  const mapTileIndex = MapSheet.getMapTileIndex(x);
+function convertTileName(fileName: string, scale: number): string | null {
+  const mapTileIndex = MapSheet.getMapTileIndex(fileName);
   if (mapTileIndex == null) return null;
   return getTileName(mapTileIndex.bbox[0], mapTileIndex.bbox[3], scale);
 }
