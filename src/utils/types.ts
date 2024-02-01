243ceebf403202a7063a99a90f044ed3f8d24f65
Victor Engmark <vengmark@linz.govt.nz>
2024-02-02 09:49:46 +1300

    docs: Explain branded types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,11 @@
+/**
+ * Branded types can't be replaced with their underlying types. This means that we can't, for example, pass a `string`
+ * when a function expects a `PathString`. This clarifies the semantics of containers (variables, parameters) while
+ * allowing us to use the values of those containers as plain strings.
+ *
+ * See <https://egghead.io/blog/using-branded-types-in-typescript> for details.
+ */
+
 declare const __brand: unique symbol;
 type Brand<B> = { [__brand]: B };
 
