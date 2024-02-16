linz/argo-tasks

commit 18576e6a246c46dd65f26dc2a24c5531af9703d6

refactor: Move default value up the stack TDE-1030 (#854)
#### Motivation

A slightly simpler implementation.

#### Checklist

- [ ] Tests updated (unnecessary)
- [ ] Docs updated (N/A)
- [x] Issue linked in Title

src/commands/common.ts
@@ -10,6 +10,7 @@ import { logger, registerLogger } from '../log.js';
 import { isArgo } from '../utils/argo.js';
 
 export const config = option({
+  defaultValue: () => process.env['AWS_ROLE_CONFIG_PATH'],
   long: 'config',
   description: 'Location of role configuration file',
   type: optional(string),
@@ -31,7 +32,7 @@ export const forceOutput = flag({
 export function registerCli(cli: { name: string }, args: { verbose?: boolean; config?: string }): void {
   cleanArgs(args);
   registerLogger(args);
-  registerFileSystem(args);
+  registerFileSystem(args.config);
 
   logger.info({ package: CliInfo, cli: cli.name, args, isArgo: isArgo() }, 'Cli:Start');
 }

src/fs.register.ts
@@ -18,13 +18,12 @@ function splitConfig(x: string): string[] {
   return x.split(',');
 }
 
-export function registerFileSystem(opts: { config?: string }): void {
+export function registerFileSystem(config?: string): void {
   fsa.register('s3://', s3Fs);
 
-  const configPath = opts.config ?? process.env['AWS_ROLE_CONFIG_PATH'];
-  if (configPath == null || configPath === '') return;
+  if (config == null || config === '') return;
 
-  const paths = splitConfig(configPath);
+  const paths = splitConfig(config);
 
   for (const path of paths) s3Fs.credentials.registerConfig(path, fsa);
 }

commit bd2c5053b004882026cd24d2692fa34d20e6c05f

revert: "refactor: Move default value up the stack TDE-1030 (#854)" (#867)
This reverts commit 18576e6a246c46dd65f26dc2a24c5531af9703d6 which
caused issues when copying files due to `registerCli` being invoked in
multiple places.

src/commands/common.ts
@@ -10,7 +10,6 @@ import { logger, registerLogger } from '../log.js';
 import { isArgo } from '../utils/argo.js';
 
 export const config = option({
-  defaultValue: () => process.env['AWS_ROLE_CONFIG_PATH'],
   long: 'config',
   description: 'Location of role configuration file',
   type: optional(string),
@@ -32,7 +31,7 @@ export const forceOutput = flag({
 export function registerCli(cli: { name: string }, args: { verbose?: boolean; config?: string }): void {
   cleanArgs(args);
   registerLogger(args);
-  registerFileSystem(args.config);
+  registerFileSystem(args);
 
   logger.info({ package: CliInfo, cli: cli.name, args, isArgo: isArgo() }, 'Cli:Start');
 }

src/fs.register.ts
@@ -18,12 +18,13 @@ function splitConfig(x: string): string[] {
   return x.split(',');
 }
 
-export function registerFileSystem(config?: string): void {
+export function registerFileSystem(opts: { config?: string }): void {
   fsa.register('s3://', s3Fs);
 
-  if (config == null || config === '') return;
+  const configPath = opts.config ?? process.env['AWS_ROLE_CONFIG_PATH'];
+  if (configPath == null || configPath === '') return;
 
-  const paths = splitConfig(config);
+  const paths = splitConfig(configPath);
 
   for (const path of paths) s3Fs.credentials.registerConfig(path, fsa);
 }