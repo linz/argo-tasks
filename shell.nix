8c7f2ce1a87346ef711f6f0219906fd81000d597
Victor Engmark <vengmark@linz.govt.nz>
2024-03-01 11:12:57 +1300

    feat: Support Nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  pkgs = import (
+    builtins.fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/d934204a0f8d9198e1e4515dd6fec76a139c87f0.tar.gz";
+      sha256 = "1zfby2jsfkag275aibp81bx1g1cc305qbcy94gqw0g6zki70k1lx";
+    }
+  ) {};
+
+in
+  pkgs.mkShell {
+    packages = [
+      pkgs.bashInteractive
+      pkgs.nodejs_20
+    ];
+    shellHook = ''
+      ln --force --no-target-directory --symbolic "${pkgs.nodejs}/bin/node" node
+    '';
+  }
