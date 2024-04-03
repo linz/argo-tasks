19165cb30e39c647a050842688baa7ee940819cf
Megan Davidson <mdavidson@linz.govt.nz>
2024-04-04 10:26:24 +1300

    chore: rename to match other function

--- src/__test__/eai_againRetry.test.ts
+++ src/__test__/eai_againRetry.test.ts
@@ -7,7 +7,7 @@ import { eaiAgainBuilder } from '../fs.register.js';
 
 let callCount = 0;
 
-function buildFakeNext(failCount: number): BuildHandler<object, MetadataBearer> {
+function fakeNextBuilder(failCount: number): BuildHandler<object, MetadataBearer> {
   const fakeNext: BuildHandler<object, MetadataBearer> = () => {
     // fail twice and then succeed
     callCount += 1;
@@ -25,19 +25,19 @@ describe('eai_againRetryMiddleware', () => {
     callCount = 0;
   });
   it('should run next once if it succeeds', () => {
-    const fakeNext = buildFakeNext(0);
+    const fakeNext = fakeNextBuilder(0);
     eaiAgainBuilder(0)(fakeNext, {})({ input: {}, request: {} });
     assert.equal(callCount, 1);
   });
 
   it('should try three times when getting EAI_AGAIN errors', async () => {
-    const fakeNext = buildFakeNext(2);
+    const fakeNext = fakeNextBuilder(2);
     await eaiAgainBuilder(0)(fakeNext, {})({ input: {}, request: {} });
     assert.equal(callCount, 3);
   });
 
   it('should throw error if next fails three times', () => {
-    const fakeNext = buildFakeNext(3);
+    const fakeNext = fakeNextBuilder(3);
     assert.rejects(eaiAgainBuilder(0)(fakeNext, {})({ input: {}, request: {} }), {
       message: 'EAI_AGAIN maximum tries (3) exceeded',
     });
