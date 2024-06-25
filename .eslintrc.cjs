f9eda7458c2adb5c7e6667061c3f97ae85e7ecf4
Blayne Chard <bchard@linz.govt.nz>
2024-06-26 10:59:50 +1200

    refactor: typo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ const testOverrides = cfg.overrides.find((ovr) => ovr.files.find((f) => f.includ
 testOverrides.rules['@typescript-eslint/no-floating-promises'] = 'off';
 
 // Disable no floating promises in tests until https://github.com/nodejs/node/issues/51292 is solved
-const tsOverrides = cfg.overrides.find((ovr) => ovr.files.find((f) => f.ends('*.ts')));
+const tsOverrides = cfg.overrides.find((ovr) => ovr.files.find((f) => f.endsWith('*.ts')));
 tsOverrides.rules['@typescript-eslint/require-await'] = 'off'
 
 module.exports = cfg;
