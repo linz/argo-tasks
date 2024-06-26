6e0d3e292cbb98771ddf831a9c110f87a0a37ea6
Blayne Chard <bchard@linz.govt.nz>
2024-06-26 14:51:25 +1200

    refactor: correct docs for why require-await is disabled

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ const cfg = {
 const testOverrides = cfg.overrides.find((ovr) => ovr.files.find((f) => f.includes('.test.ts')));
 testOverrides.rules['@typescript-eslint/no-floating-promises'] = 'off';
 
-// Disable no floating promises in tests until https://github.com/nodejs/node/issues/51292 is solved
+// Async functions are required in a few places without awaits eg `async foo() { throw Bar }`
 const tsOverrides = cfg.overrides.find((ovr) => ovr.files.find((f) => f.endsWith('*.ts')));
 tsOverrides.rules['@typescript-eslint/require-await'] = 'off'
 
