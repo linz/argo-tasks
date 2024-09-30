3452eefe9ab97d832958a2a4649dfaac4d9efbc3
Blayne Chard <bchard@linz.govt.nz>
2024-10-01 12:08:17 +1300

    fix(copy): add threadId to all logs created when running from threads

--- src/log.ts
+++ src/log.ts
@@ -2,12 +2,11 @@ import { pino } from 'pino';
 import { PrettyTransform } from 'pretty-json-log';
 import ulid from 'ulid';
 
-export const baseLogger = process.stdout.isTTY ? pino(PrettyTransform.stream()) : pino();
-baseLogger.level = 'debug';
+export const logger = process.stdout.isTTY ? pino(PrettyTransform.stream()) : pino();
+logger.level = 'debug';
 
 export const logId = ulid.ulid();
-
-export const logger = baseLogger.child({ id: logId });
+logger.setBindings({ id: logId });
 
 export function registerLogger(cfg: { verbose?: boolean }): void {
   if (cfg.verbose) logger.level = 'trace';
