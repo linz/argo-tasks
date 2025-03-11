052d4db5a859584ea00ed5f61d3debb5b12fcd16
Blayne Chard <bchard@linz.govt.nz>
2025-03-11 13:20:59 +1300

    refactor: cleanup unused code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ testOverrides.rules['@typescript-eslint/no-floating-promises'] = 'off';
 const tsOverrides = cfg.overrides.find((ovr) => ovr.files.find((f) => f.endsWith('*.ts')));
 tsOverrides.rules['@typescript-eslint/require-await'] = 'off';
 
+// node23 needs "import type" when importing types
 tsOverrides.rules['@typescript-eslint/consistent-type-imports'] = 'error';
 
 module.exports = cfg;
