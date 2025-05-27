b62240cec1c342b10c7bbccebd39b6e521842a15
paulfouquet <86932794+paulfouquet@users.noreply.github.com>
2025-05-28 09:32:50 +1200

    fix: package information is not logged when command has not started TDE-1511 (#1226)
    #### Motivation
    
    If a command fails to start (for example, missing arguments), the
    package information (for example,
    `"package":{"package":"@linzjs/argo-tasks","version":"v4.14.2","hash":"e8dbf80c51b7a2baf61b22eec49de7072c812223","buildId":""}`)
    is not logged. Without this information, it makes it difficult to
    reproduce.
    
    #### Modification
    
    - Move package info logs before the command run
    
    #### Verification
    
    Local test

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ process.env['AWS_NODEJS_CONNECTION_REUSE_ENABLED'] = '1';
 
 import { run } from 'cmd-ts';
 
+import { CliInfo } from './cli.info.ts';
 import { cmd } from './commands/index.ts';
 import { logger } from './log.ts';
 
 const startTime = performance.now();
+logger.info({ package: CliInfo, args: process.argv.slice(2) }, 'Cli:Info');
 run(cmd, process.argv.slice(2))
   .then(() => {
     logger.debug({ duration: performance.now() - startTime }, 'Command:Done');
