6327863a5fecaacfa76f1982c4f31b068413ce03
Tobias Schmidt <13055656+schmidtnz@users.noreply.github.com>
2025-08-27 13:48:16 +1200

    fix: re-order imports

--- src/commands/stac-catalog/__test__/stac.catalog.test.ts
+++ src/commands/stac-catalog/__test__/stac.catalog.test.ts
@@ -4,8 +4,8 @@ import { beforeEach, describe, it } from 'node:test';
 import { fsa, FsMemory } from '@chunkd/fs';
 import { pathToFileURL } from 'url';
 
-import { createLinks } from '../stac.catalog.ts';
 import { makeRelative } from '../../../utils/filelist.ts';
+import { createLinks } from '../stac.catalog.ts';
 
 describe('stacCatalog', () => {
   const fs = new FsMemory();
