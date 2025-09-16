48bcf03e192129ea4b31aafc4e815ca3a75df69d
Alice Fage <afage@linz.govt.nz>
2025-09-17 09:23:27 +1200

    fix: remove unused import

--- src/fs.register.ts
+++ src/fs.register.ts
@@ -1,7 +1,7 @@
 import { setTimeout } from 'node:timers/promises';
 
 import { S3Client } from '@aws-sdk/client-s3';
-import { fsa, FsHttp } from '@chunkd/fs';
+import { fsa } from '@chunkd/fs';
 import type { AwsCredentialConfig } from '@chunkd/fs-aws';
 import { AwsS3CredentialProvider, FsAwsS3 } from '@chunkd/fs-aws';
 import type { BuildMiddleware, FinalizeRequestMiddleware, MetadataBearer } from '@smithy/types';
