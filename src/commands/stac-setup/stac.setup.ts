2987a2c51d815bbe531a99f60d40c851b8698339
Sheetal Poudel <spoudel@linz.govt.nz>
2026-09-03 11:04:28 +1200

    fix: loads tiff once for both gsd and datatype instead of repeating TDE-2032

--- src/commands/stac-setup/stac.setup.ts
+++ src/commands/stac-setup/stac.setup.ts
@@ -1,4 +1,5 @@
 import { fsa } from '@chunkd/fs';
+import type { Tiff } from '@cogeotiff/core';
 import { command, option, optional, string } from 'cmd-ts';
 import type { StacCollection } from 'stac-ts';
 import ulid from 'ulid';
@@ -121,16 +122,16 @@ export const commandStacSetup = command({
       const slug = collection['linz:slug'];
       if (slug !== slugify(slug)) throw new Error(`Invalid slug: ${slug}.`);
 
-      const gsd =
-        Number(collection['gsd']) || (await loadFirstTiff(collectionLocation, collection)).images[0]?.resolution[0];
+      let tiff: Tiff | undefined;
+      const getTiff = async (): Promise<Tiff> => (tiff ??= await loadFirstTiff(collectionLocation, collection));
+
+      const gsd = Number(collection['gsd']) || (await getTiff()).images[0]?.resolution[0];
       if (gsd !== Number(args.gsd)) {
         logger.error({ gsd, expected: args.gsd }, 'StacSetup:Error:GSDMismatch');
         throw new Error(`GSD at ODR URL [${gsd}] does not match new TIFF GSD [${args.gsd}]`);
       }
 
-      const dataType =
-        collection['data_type'] ??
-        (await extractBandInformation(await loadFirstTiff(collectionLocation, collection)))[0];
+      const dataType = collection['data_type'] ?? (await extractBandInformation(await getTiff()))[0];
       if (dataType !== args.dataType) {
         logger.error({ dataType, expected: args.dataType }, 'StacSetup:Error:DataTypeMismatch');
         throw new Error(`Data type at ODR URL [${dataType}] does not match new TIFF data type [${args.dataType}]`);
